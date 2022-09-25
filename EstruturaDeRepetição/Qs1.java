29423f8c12e06d904f2943f54a9b0dc8cedefe85
carlosravick <barra.carlosfilho@hotmail.com>
2022-09-25 02:11:01 -0300

    Atualizando atv do JDFortaleza

---
+++ EstruturaDeRepetição/Qs1.java
@@ -0,0 +1,17 @@
+package EstruturaDeRepetição;
+
+import java.util.Scanner;
+
+public class Qs1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leitor = new Scanner(System.in);
+		
+		for (int i = 1; i<=20;i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
---
+++ EstruturaDeRepetição/Qs2.java
@@ -0,0 +1,16 @@
+package EstruturaDeRepetição;
+
+import java.util.Scanner;
+
+public class Qs2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leitor = new Scanner(System.in);
+		
+		for (int i = 1; i<=30 ; i++) {
+			System.out.println(i + "+" + i  + " = " + (i+i));
+		}
+	}
+
+}
